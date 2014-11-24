3631b11918c6391d5789747ab27a60aebf42dd4a
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-23 21:57:29 -0500

    Copying MainActivity.java

--- app/src/main/java/com/detroitlabs/kyleofori/connectapi/activities/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/connectapi/activities/MainActivity.java
@@ -1,50 +1,80 @@
 package com.detroitlabs.kyleofori.connectapi.activities;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ListView;
+import android.app.Activity;
+import android.app.AlertDialog;
 
 import com.detroitlabs.kyleofori.connectapi.R;
-import com.detroitlabs.kyleofori.connectapi.fragments.MainFragment;
-
 
 public class MainActivity extends Activity {
 
+    AlertDialog alertDialogStores;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainFragment())
-                    .commit();
-        }
+
+        // a button to show the pop up with a list view
+        View.OnClickListener handler = new View.OnClickListener(){
+            public void onClick(View v) {
+                switch (v.getId()) {
+
+                    case R.id.buttonShowPopUp:
+                        showPopUp();
+                        break;
+                }
+            }
+        };
+
+        findViewById(R.id.buttonShowPopUp).setOnClickListener(handler);
+
     }
 
+    public void showPopUp(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // add your items, this can be done programatically
+        // your items can be from a database
+        ObjectItem[] ObjectItemData = new ObjectItem[20];
+
+        ObjectItemData[0] = new ObjectItem(91, "Mercury");
+        ObjectItemData[1] = new ObjectItem(92, "Watson");
+        ObjectItemData[2] = new ObjectItem(93, "Nissan");
+        ObjectItemData[3] = new ObjectItem(94, "Puregold");
+        ObjectItemData[4] = new ObjectItem(95, "SM");
+        ObjectItemData[5] = new ObjectItem(96, "7 Eleven");
+        ObjectItemData[6] = new ObjectItem(97, "Ministop");
+        ObjectItemData[7] = new ObjectItem(98, "Fat Chicken");
+        ObjectItemData[8] = new ObjectItem(99, "Master Siomai");
+        ObjectItemData[9] = new ObjectItem(100, "Mang Inasal");
+        ObjectItemData[10] = new ObjectItem(101, "Mercury 2");
+        ObjectItemData[11] = new ObjectItem(102, "Watson 2");
+        ObjectItemData[12] = new ObjectItem(103, "Nissan 2");
+        ObjectItemData[13] = new ObjectItem(104, "Puregold 2");
+        ObjectItemData[14] = new ObjectItem(105, "SM 2");
+        ObjectItemData[15] = new ObjectItem(106, "7 Eleven 2");
+        ObjectItemData[16] = new ObjectItem(107, "Ministop 2");
+        ObjectItemData[17] = new ObjectItem(108, "Fat Chicken 2");
+        ObjectItemData[18] = new ObjectItem(109, "Master Siomai 2");
+        ObjectItemData[19] = new ObjectItem(110, "Mang Inasal 2");
+
+        // our adapter instance
+        ArrayAdapterItem adapter = new ArrayAdapterItem(this, R.layout.list_view_row_item, ObjectItemData);
+
+        // create a new ListView, set the adapter and item click listener
+        ListView listViewItems = new ListView(this);
+        listViewItems.setAdapter(adapter);
+        listViewItems.setOnItemClickListener(new OnItemClickListenerListViewItem());
+
+        // put the ListView in the pop up
+        alertDialogStores = new AlertDialog.Builder(MainActivity.this)
+                .setView(listViewItems)
+                .setTitle("Stores")
+                .show();
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
